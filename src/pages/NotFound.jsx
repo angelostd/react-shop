3aebb2505c09a7d3e5b8b0ec263c4924c362d6d7
Angel Duarte <angel.dtd.esc@gmail.com>
2023-01-23 19:01:18 -0600

    Before React.useState() 😸

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/NotFound.scss';
+
+const NotFound = () => {
+    return (
+        <div className='container__not-found'>
+            <p><span>Error 404</span><br />Not Found!</p>
+        </div>
+    );
+}
+
+export default NotFound;
