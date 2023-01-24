37945b0d3308419beb8c81703c47247ddd5fa654
Angel Duarte <angel.dtd.esc@gmail.com>
2023-01-23 20:35:54 -0600

    use[State, Effect, Ref]() impl 😸

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Menu from '@components/Menu';
 import '@styles/Header.scss'
 import iconMenu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
@@ -6,6 +7,14 @@ import iconShoppingCart from '@icons/icon_shopping_cart.svg';
 
 
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = (e) => {
+        e.preventDefault();
+        setToggle(!toggle);
+    }
+
     return (
         <nav>
             <img src={iconMenu} alt="menu" className="menu" />
@@ -34,13 +43,16 @@ const Header = () => {
             </div>
             <div className="navbar-right">
                 <ul>
-                    <li className="navbar-email">platzi@example.com</li>
+                    <li className="navbar-email" onClick={handleToggle}>
+                        platzi@example.com
+                    </li>
                     <li className="navbar-shopping-cart">
                         <img src={iconShoppingCart} alt="shopping cart" />
                         <div>2</div>
                     </li>
                 </ul>
             </div>
+            {toggle ? <Menu /> : null}
         </nav>
 
     );
